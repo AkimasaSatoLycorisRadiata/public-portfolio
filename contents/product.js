1ca446053626af68d7e20291b1ac48f41b341075
Akimasa.Sato <novemberist.straightener.8492@gmail.com>
2024-09-07 19:09:18 +0900

    Commit_20240907_04 js実装(フェードイン)

---
+++ contents/product.js
@@ -0,0 +1,13 @@
+/*メインコンテンツのフェードイン*/
+var fade_in = document.getElementById("fade-in");
+
+fade_in.parentElement.opacity = "0";
+
+fade_in.style.opacity = "0";
+
+fade_in.style.transition = "opacity 1s ease-in-out";
+
+window.addEventListener("load", function() {
+    fade_in.style.opacity = "1";
+    fade_in.parentElement.opacity = "1";   
+});
