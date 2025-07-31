89744fbaa5e0f86a65d0c5b30bf2a6322cb90a93
Akimasa.Sato <novemberist.straightener.8492@gmail.com>
2025-07-31 23:53:21 +0900

    Productページを再度構成

--- product/product.js
+++ product/product.js
@@ -1,7 +1,9 @@
-// フェードイン効果
 document.addEventListener("DOMContentLoaded", () => {
-    const fade_in = document.getElementById("fade-in");
-    if (fade_in) {
-        fade_in.style.opacity = "1";
-    }
+    const fadeIn = document.getElementById("fade-in");
+    fadeIn.style.opacity = "0";
+    fadeIn.style.transition = "opacity 1s ease-in-out";
+
+    requestAnimationFrame(() => {
+        fadeIn.style.opacity = "1";
+    });
 });
